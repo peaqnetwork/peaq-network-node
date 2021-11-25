fea78b3f628c03641150b0620eb791650316f21d
Iredia Ebikade <iredia.developer@gmail.com>
2021-11-25 22:11:51 +0100

    feat: added update attribute unit tests

--- pallets/did/src/tests.rs
+++ pallets/did/src/tests.rs
@@ -1,6 +1,5 @@
 use crate::{did::Did, mock::*, Error};
 use frame_support::{assert_noop, assert_ok};
-use frame_system as system;
 
 #[test]
 fn add_attribute_test() {
@@ -20,6 +19,7 @@ fn add_attribute_test() {
 		));
 		assert_eq!(PeaqDID::nonce_of((identity, name.to_vec())), 1);
 
+		// Test for duplicate entry
 		assert_ok!(PeaqDID::add_attribute(
 			Origin::signed(identity),
 			name.to_vec(),
@@ -28,5 +28,44 @@ fn add_attribute_test() {
 		));
 
 		assert_eq!(PeaqDID::nonce_of((identity, name.to_vec())), 2);
+	});
+}
+
+#[test]
+fn update_attribute_test() {
+	new_test_ext().execute_with(|| {
+		let acct = "Iredia";
+		let identity = account_key(acct);
+		let name = b"id";
+		let attribute = b"did:pq:1234567890";
+
+		assert_eq!(PeaqDID::nonce_of((identity, name.to_vec())), 0);
+
+		assert_ok!(PeaqDID::add_attribute(
+			Origin::signed(identity),
+			name.to_vec(),
+			attribute.to_vec(),
+			None
+		));
+		assert_eq!(PeaqDID::nonce_of((identity, name.to_vec())), 1);
+
+		assert_ok!(PeaqDID::update_attribute(
+			Origin::signed(identity),
+			name.to_vec(),
+			attribute.to_vec(),
+			None,
+			0
+		));
+
+		assert_noop!(
+			PeaqDID::update_attribute(
+				Origin::signed(identity),
+				name.to_vec(),
+				attribute.to_vec(),
+				None,
+				100
+			),
+			Error::<Test>::AttributeNotFound
+		);
 	});
 }
