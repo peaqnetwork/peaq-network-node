8006f24c8e934776cec9602dfbade34e4f1ebe6d
jaypan <pan@advancedblockchain.com>
2022-03-10 10:48:27 +0100

    Import more

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -23,7 +23,9 @@ use sp_runtime::{
 		AccountIdLookup, BlakeTwo256, Block as BlockT, Dispatchable, IdentifyAccount, NumberFor,
 		PostDispatchInfoOf, Verify,
 	},
-	transaction_validity::{TransactionSource, TransactionValidity, TransactionValidityError},
+	transaction_validity::{
+		TransactionSource, TransactionValidity, TransactionValidityError, InvalidTransaction
+	},
 	ApplyExtrinsicResult, MultiSignature,
 };
 use sp_std::{marker::PhantomData, prelude::*};
@@ -40,6 +42,7 @@ pub use frame_support::{
 	weights::{
 		constants::{BlockExecutionWeight, ExtrinsicBaseWeight, RocksDbWeight, WEIGHT_PER_SECOND},
 		IdentityFee, Weight,
+		GetDispatchInfo
 	},
 	ConsensusEngineId, StorageValue,
 };
@@ -535,9 +538,11 @@ impl fp_self_contained::SelfContainedCall for Call {
 		}
 	}
 
-	fn validate_self_contained(&self, info: &Self::SignedInfo) -> Option<TransactionValidity> {
+	fn validate_self_contained(&self, signed_info: &Self::SignedInfo) -> Option<TransactionValidity> {
 		match self {
-			Call::Ethereum(call) => call.validate_self_contained(info),
+			Call::Ethereum(ref call) => {
+				Some(validate_self_contained_inner(&self, &call, signed_info))
+			}
 			_ => None,
 		}
 	}
@@ -562,6 +567,41 @@ impl fp_self_contained::SelfContainedCall for Call {
 			)),
 			_ => None,
 		}
+	}
+}
+
+fn validate_self_contained_inner(
+	call: &Call,
+	eth_call: &pallet_ethereum::Call<Runtime>,
+	signed_info: &<Call as fp_self_contained::SelfContainedCall>::SignedInfo
+) -> TransactionValidity {
+	if let pallet_ethereum::Call::transact { ref transaction } = eth_call {
+		// Previously, ethereum transactions were contained in an unsigned
+		// extrinsic, we now use a new form of dedicated extrinsic defined by
+		// frontier, but to keep the same behavior as before, we must perform
+		// the controls that were performed on the unsigned extrinsic.
+		use sp_runtime::traits::SignedExtension as _;
+		let input_len = match transaction {
+			pallet_ethereum::Transaction::Legacy(t) => t.input.len(),
+			pallet_ethereum::Transaction::EIP2930(t) => t.input.len(),
+			pallet_ethereum::Transaction::EIP1559(t) => t.input.len(),
+		};
+		let extra_validation = SignedExtra::validate_unsigned(
+			call,
+			&call.get_dispatch_info(),
+			input_len,
+		)?;
+		// Then, do the controls defined by the ethereum pallet.
+		// use fp_self_contained::SelfContainedCall as _;
+		let self_contained_validation = eth_call
+			.validate_self_contained(signed_info)
+			.ok_or(TransactionValidityError::Invalid(InvalidTransaction::BadProof))??;
+
+		Ok(extra_validation.combine_with(self_contained_validation))
+	} else {
+		Err(TransactionValidityError::Unknown(
+			sp_runtime::transaction_validity::UnknownTransaction::CannotLookup
+		))
 	}
 }
 
