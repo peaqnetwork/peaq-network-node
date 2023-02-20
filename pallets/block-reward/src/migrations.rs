66be5b03d253560bd2521fc8d6cd4f653c66bebe
Michael Eberhardt <github@michael-eberhardt.de>
2023-02-20 11:45:18 +0100

    changed version, instead 3.0.0 we do 2.1.0

--- pallets/block-reward/src/migrations.rs
+++ pallets/block-reward/src/migrations.rs
@@ -12,34 +12,34 @@ use frame_support::{
 // migration logic. This internal storage version is independent to branch/crate versions.
 #[derive(Encode, Decode, Clone, Copy, PartialEq, Eq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
 pub enum StorageReleases {
-    V0_0_0,
-	V3_0_0, // First changes compared to releases before, renaming HardCap to MaxCurrencySupply
+    V2_0_0,
+	V2_1_0, // First changes compared to releases before, renaming HardCap to MaxCurrencySupply
 }
 
 impl Default for StorageReleases {
 	fn default() -> Self {
-		StorageReleases::V0_0_0
+		StorageReleases::V2_0_0
 	}
 }
 
 
 pub(crate) fn on_runtime_upgrade<T: Config>() -> Weight {
-    v3::MigrateToV3::<T>::on_runtime_upgrade()
+    v2::MigrateToV2x::<T>::on_runtime_upgrade()
 }
 
 
-mod v3 {
+mod v2 {
     use super::*;
 
     #[storage_alias]
 	type HardCap<T: Config> = StorageValue<Pallet<T>, BalanceOf<T>, ValueQuery>;
 
     /// Migration implementation that renames storage HardCap into MaxCurrencySupply
-    pub struct MigrateToV3<T>(sp_std::marker::PhantomData<T>);
+    pub struct MigrateToV2x<T>(sp_std::marker::PhantomData<T>);
 
-    impl<T: Config> MigrateToV3<T> {
+    impl<T: Config> MigrateToV2x<T> {
         pub fn on_runtime_upgrade() -> Weight {
-            if VersionStorage::<T>::get() != StorageReleases::V0_0_0 {
+            if VersionStorage::<T>::get() != StorageReleases::V2_0_0 {
                 T::DbWeight::get().reads(1)
             } else {
                 log!(info, "Migrating block_reward to Releases::V3_0_0");
@@ -47,9 +47,9 @@ mod v3 {
                 let storage = HardCap::<T>::get();
                 MaxCurrencySupply::<T>::put(storage);
                 HardCap::<T>::kill();
-                VersionStorage::<T>::put(StorageReleases::V3_0_0);
+                VersionStorage::<T>::put(StorageReleases::V2_1_0);
 
-                log!(info, "Releases::V3_0_0 Migrating Done.");
+                log!(info, "Releases::V2_1_0 Migrating Done.");
 
                 T::DbWeight::get().reads_writes(1, 2)
             }
