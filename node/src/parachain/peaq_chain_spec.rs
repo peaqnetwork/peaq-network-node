ab48f7f257b9e76ee1411c68216b363807d8721a
jaypan <pan@peaq.io>
2023-02-28 16:47:35 +0100

    Update the endow account's balance

--- node/src/parachain/peaq_chain_spec.rs
+++ node/src/parachain/peaq_chain_spec.rs
@@ -144,8 +144,8 @@ fn configure_genesis(
 		},
 		parachain_info: ParachainInfoConfig { parachain_id },
 		balances: BalancesConfig {
-			// Configure endowed accounts with initial balance of 1 << 78.
-			balances: endowed_accounts.iter().cloned().map(|k| (k, 1 << 78)).collect(),
+			// Configure endowed accounts with initial balance of 1 << 62.
+			balances: endowed_accounts.iter().cloned().map(|k| (k, 1 << 62)).collect(),
 		},
 		session: peaq_runtime::SessionConfig {
 			keys: initial_authorities
