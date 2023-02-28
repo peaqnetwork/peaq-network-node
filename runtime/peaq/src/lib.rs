184fe3c2aae669a22f70d6f2641c5fc96681f2c5
jaypan <pan@peaq.io>
2023-02-28 11:00:22 +0100

    Add treasury related distribution

--- runtime/peaq/src/lib.rs
+++ runtime/peaq/src/lib.rs
@@ -748,6 +748,7 @@ impl cumulus_pallet_aura_ext::Config for Runtime {}
 
 parameter_types! {
 	pub const PotStakeId: PalletId = PalletId(*b"PotStake");
+	pub const PotTreasuryId: PalletId = TreasuryPalletId::get();
 }
 
 parameter_types! {
@@ -868,6 +869,14 @@ macro_rules! impl_to_pot_adapter {
 
 impl_to_pot_adapter!(ToStakingPot, PotStakeId, NegativeImbalance);
 
+pub struct ToTreasuryPot;
+impl OnUnbalanced<NegativeImbalance> for ToTreasuryPot {
+	fn on_nonzero_unbalanced(amount: NegativeImbalance) {
+		let pot = PotTreasuryId::get().into_account_truncating();
+		Balances::resolve_creating(&pot, amount);
+	}
+}
+
 impl pallet_block_reward::Config for Runtime {
 	type Currency = Balances;
 	type BeneficiaryPayout = BeneficiaryPayout;
@@ -877,7 +886,9 @@ impl pallet_block_reward::Config for Runtime {
 
 pub struct BeneficiaryPayout();
 impl pallet_block_reward::BeneficiaryPayout<NegativeImbalance> for BeneficiaryPayout {
-	fn treasury(_reward: NegativeImbalance) {}
+	fn treasury(reward: NegativeImbalance) {
+		ToTreasuryPot::on_unbalanced(reward);
+	}
 
 	fn collators(reward: NegativeImbalance) {
 		ToStakingPot::on_unbalanced(reward);
@@ -904,7 +915,10 @@ impl orml_currencies::Config for Runtime {
 }
 
 pub fn get_all_module_accounts() -> Vec<AccountId> {
-	vec![PotStakeId::get().into_account_truncating()]
+	vec![
+		PotStakeId::get().into_account_truncating(),
+		PotTreasuryId::get().into_account_truncating(),
+	]
 }
 
 pub struct DustRemovalWhitelist;
