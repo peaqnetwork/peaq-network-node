be0a51660dfad9dc212c916140b41063e49bf882
jaypan <pan@peaq.io>
2023-04-07 05:59:42 +0200

    Allow delegator/collator added

--- runtime/krest/src/lib.rs
+++ runtime/krest/src/lib.rs
@@ -248,11 +248,6 @@ pub struct BaseFilter;
 impl Contains<Call> for BaseFilter {
 	fn contains(call: &Call) -> bool {
 		match call {
-			Call::ParachainStaking(method) => !matches!(
-				method,
-				parachain_staking::Call::join_candidates { .. } |
-					parachain_staking::Call::join_delegators { .. }
-			),
 			// Other modules should works:
 			_ => true,
 		}
