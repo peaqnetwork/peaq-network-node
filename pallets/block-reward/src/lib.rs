270808060eb6f7316913124427358a0e1a353944
Michael Eberhardt <github@michael-eberhardt.de>
2023-10-04 13:53:23 +0400

    removed example code, because cargo fmt won't stop touching it, bad fmtgit add pallets/

--- pallets/block-reward/src/lib.rs
+++ pallets/block-reward/src/lib.rs
@@ -38,37 +38,7 @@
 //! ## Usage
 //!
 //! 1. Pallet should be set as a handler of `OnTimestampSet`.
-//! 2. `BeneficiaryPayout` handler should be defined as an impl of `BeneficiaryPayout` trait. For
-//!    example:
-//!    ```ignore
-//!    pub struct BeneficiaryPayout();
-//!
-//!    impl BeneficiaryPayout<NegativeImbalanceOf<T>> for BeneficiaryPayout {
-//!        fn treasury(reward: NegativeImbalanceOf<T>) {
-//!            Balances::resolve_creating(&TREASURY_POT.into_account(), reward);
-//!        }
-//!
-//!        fn collators(reward: NegativeImbalanceOf<T>) {
-//!            Balances::resolve_creating(&COLLATOR_POT.into_account(), reward);
-//!        }
-//!
-//!        fn dapps_staking(reward: NegativeImbalanceOf<T>) {
-//!            DappsStaking::rewards(reward);
-//!        }
-//!
-//!        fn lp_users(reward: Imbalance) {
-//!            // ...
-//!        }
-//!
-//!        fn machines(reward: Imbalance) {
-//!            // ...
-//!        }
-//!
-//!        fn parachain_lease_fund(reward: Imbalance) {
-//!            // ...
-//!        }
-//!    }
-//!    ```
+//! 2. `BeneficiaryPayout` handler should be defined as an impl of `BeneficiaryPayout` trait.
 //! 3. Set `RewardAmount` to desired block reward value in the genesis configuration.
 //! 4. Set `MaxCurrencySupply` to limit maximum currency supply in the genesis configuration.
 
