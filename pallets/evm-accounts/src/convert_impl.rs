2f179fc446183b2d8e1db34c7c6c29eb6a37e718
jaypan <pan@peaq.io>
2023-11-23 15:24:42 +0100

    Remove uncenssary generic

--- pallets/evm-accounts/src/convert_impl.rs
+++ pallets/evm-accounts/src/convert_impl.rs
@@ -2,7 +2,7 @@ use crate::{Accounts, Config, EvmAddresses, H160};
 use pallet_evm::AddressMapping as PalletEVMAddressMapping;
 use parity_scale_codec::Encode;
 use peaq_primitives_xcm::EvmAddress;
-use sp_core::{Hasher, H256};
+use sp_io::hashing::blake2_256;
 
 use sp_std::marker::PhantomData;
 
@@ -14,13 +14,12 @@ pub trait UnifyAddressMapping<AccountId> {
 	fn to_default_evm_address(account_id: &AccountId) -> EvmAddress;
 }
 
-pub struct UnifyAddressMapper<T, H>(PhantomData<(T, H)>);
+pub struct UnifyAddressMapper<T>(PhantomData<T>);
 
-impl<T, H> UnifyAddressMapping<T::AccountId> for UnifyAddressMapper<T, H>
+impl<T> UnifyAddressMapping<T::AccountId> for UnifyAddressMapper<T>
 where
 	T: Config,
 	T::OriginAddressMapping: PalletEVMAddressMapping<T::AccountId>,
-	H: Hasher<Out = H256>,
 {
 	/// Returns the AccountId used go generate the given EvmAddress.
 	fn to_set_account_id(evm_address: &EvmAddress) -> Option<T::AccountId> {
@@ -37,6 +36,6 @@ where
 
 	fn to_default_evm_address(account_id: &T::AccountId) -> EvmAddress {
 		let payload = (b"evm:", account_id);
-		H160::from_slice(&payload.using_encoded(H::hash)[0..20])
+		H160::from_slice(&payload.using_encoded(blake2_256)[0..20])
 	}
 }
