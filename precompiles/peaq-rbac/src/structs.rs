1b96736956e7ba2e5a4a0ead876fe4ce870ee6d3
talhadaar <muhammadtalhadar@gmail.com>
2023-12-13 13:05:55 +0500

    fixed import errors in rbac-precompile/structs

--- precompiles/peaq-rbac/src/structs.rs
+++ precompiles/peaq-rbac/src/structs.rs
@@ -1,32 +1,32 @@
-use precompile_utils::{data::String, prelude::UnboundedBytes, EvmData};
+use precompile_utils::{prelude::UnboundedBytes, solidity::Codec};
 use sp_core::H256;
 
-#[derive(EvmData)]
+#[derive(Default, Debug, Codec)]
 pub struct Entity {
 	pub id: H256,
 	pub name: UnboundedBytes,
 	pub enabled: bool,
 }
 
-#[derive(EvmData)]
+#[derive(Default, Debug, Codec)]
 pub struct Role2User {
 	pub role: H256,
 	pub user: H256,
 }
 
-#[derive(EvmData)]
+#[derive(Default, Debug, Codec)]
 pub struct Permission2Role {
 	pub permission: H256,
 	pub role: H256,
 }
 
-#[derive(EvmData)]
+#[derive(Default, Debug, Codec)]
 pub struct Role2Group {
 	pub role: H256,
 	pub group: H256,
 }
 
-#[derive(EvmData)]
+#[derive(Default, Debug, Codec)]
 pub struct User2Group {
 	pub user: H256,
 	pub group: H256,
