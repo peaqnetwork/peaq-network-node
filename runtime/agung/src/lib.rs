b31324c112132795591a1d9685b7b3f591275ae0
jaypan <pan@peaq.io>
2024-02-01 12:14:47 +0100

    Remove old migrations + new migrations

--- runtime/agung/src/lib.rs
+++ runtime/agung/src/lib.rs
@@ -494,31 +494,6 @@ parameter_types! {
 	pub MaxProposalWeight: Weight = Perbill::from_percent(50) * RuntimeBlockWeights::get().max_block;
 }
 
-#[cfg(feature = "try-runtime")]
-use sp_runtime::TryRuntimeError;
-
-const COUNCIL_OLD_PREFIX: &str = "Instance1Collective";
-/// Migrate from `Instance1Collective` to the new pallet prefix `Council`
-pub struct CouncilStoragePrefixMigration;
-
-impl frame_support::traits::OnRuntimeUpgrade for CouncilStoragePrefixMigration {
-	fn on_runtime_upgrade() -> frame_support::weights::Weight {
-		pallet_collective::migrations::v4::migrate::<Runtime, Council, _>(COUNCIL_OLD_PREFIX)
-	}
-
-	#[cfg(feature = "try-runtime")]
-	fn pre_upgrade() -> Result<Vec<u8>, TryRuntimeError> {
-		pallet_collective::migrations::v4::pre_migrate::<Council, _>(COUNCIL_OLD_PREFIX);
-		Ok(Vec::new())
-	}
-
-	#[cfg(feature = "try-runtime")]
-	fn post_upgrade(_: Vec<u8>) -> Result<(), TryRuntimeError> {
-		pallet_collective::migrations::v4::post_migrate::<Council, _>(COUNCIL_OLD_PREFIX);
-		Ok(())
-	}
-}
-
 type CouncilCollective = pallet_collective::Instance1;
 impl pallet_collective::Config<CouncilCollective> for Runtime {
 	type RuntimeOrigin = RuntimeOrigin;
@@ -1031,16 +1006,8 @@ pub type Executive = frame_executive::Executive<
 	Runtime,
 	AllPalletsWithSystem,
 	(
-		cumulus_pallet_dmp_queue::migration::Migration<Runtime>,
-		cumulus_pallet_xcmp_queue::migration::Migration<Runtime>,
-		pallet_balances::migration::MigrateToTrackInactive<
-			Runtime,
-			xcm_config::DummyCheckingAccount,
-		>,
-		pallet_contracts::Migration<Runtime>,
-		pallet_xcm::migration::v1::MigrateToV1<Runtime>,
-		pallet_multisig::migrations::v1::MigrateToV1<Runtime>,
-		CouncilStoragePrefixMigration,
+		xc_asset_config::migrations::MigrationXcmV3<Runtime>,
+		pallet_assets::migration::v1::MigrateToV1<Runtime>,
 	),
 >;
 
@@ -1715,7 +1682,6 @@ impl_runtime_apis! {
 			Contracts::get_storage(address, key)
 		}
 	}
-
 
 	impl peaq_pallet_did_runtime_api::PeaqDIDApi<Block, AccountId, BlockNumber, Moment> for Runtime {
 		fn read(did_account: AccountId, name: Vec<u8>) -> Option<
