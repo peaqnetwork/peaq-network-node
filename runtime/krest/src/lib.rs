62f8c959ef6b8177bc59dc1e9b52b148a1c98ef2
DrPing <drping974@proton.me>
2024-04-24 15:26:07 +0400

    chore(fee): Increase transaction fees

--- runtime/krest/src/lib.rs
+++ runtime/krest/src/lib.rs
@@ -420,9 +420,7 @@ pub struct WeightToFee;
 impl WeightToFeePolynomial for WeightToFee {
 	type Balance = Balance;
 	fn polynomial() -> WeightToFeeCoefficients<Self::Balance> {
-		// in Rococo, extrinsic base weight (smallest non-zero weight) is mapped to 1 MILLICENTS:
-		// in our template, we map to 1/10 of that, or 1/10 MILLICENTS
-		let p = MILLICENTS / 10;
+		let p = MILLICENTS * 54000;
 		let q = 100 * Balance::from(ExtrinsicBaseWeight::get().ref_time());
 		smallvec![WeightToFeeCoefficient {
 			degree: 1,
