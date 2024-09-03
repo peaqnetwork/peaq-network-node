7c484e71ceb8ecdf042996143a6a6d4d93b207b3
jaypan <pan@peaq.io>
2024-09-03 12:36:26 +0200

    Remove without_storage_info

--- pallets/xc-asset-config/src/lib.rs
+++ pallets/xc-asset-config/src/lib.rs
@@ -87,7 +87,6 @@ pub mod pallet {
 
 	#[pallet::pallet]
 	#[pallet::storage_version(STORAGE_VERSION)]
-	#[pallet::without_storage_info]
 	pub struct Pallet<T>(PhantomData<T>);
 
 	/// Defines conversion between asset Id and cross-chain asset location
