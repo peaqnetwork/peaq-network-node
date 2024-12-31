830ec3eb91f3f7baedae9c5ae3e6e83781ec5a8d
jaypan <pan@eotlabs.io>
2024-12-31 09:46:08 +0100

    fix pallet-evm-precompile-parachain-staking

--- precompiles/parachain-staking/src/mock.rs
+++ precompiles/parachain-staking/src/mock.rs
@@ -102,6 +102,11 @@ impl frame_system::Config for Test {
 	type OnSetCode = ();
 	type MaxConsumers = frame_support::traits::ConstU32<16>;
 	type RuntimeTask = ();
+	type SingleBlockMigrations = ();
+	type MultiBlockMigrator = ();
+	type PreInherents = ();
+	type PostInherents = ();
+	type PostTransactions = ();
 }
 
 parameter_types! {
