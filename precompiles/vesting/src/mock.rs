7dda80d5ad039b145ca2c0833efa58e783f7033a
jaypan <pan@eotlabs.io>
2025-01-02 07:31:59 +0100

    Fix precompiles/vesting

--- precompiles/vesting/src/mock.rs
+++ precompiles/vesting/src/mock.rs
@@ -82,6 +82,11 @@ impl frame_system::Config for Runtime {
 	type OnSetCode = ();
 	type MaxConsumers = frame_support::traits::ConstU32<16>;
 	type RuntimeTask = ();
+	type SingleBlockMigrations = ();
+	type MultiBlockMigrator = ();
+	type PreInherents = ();
+	type PostInherents = ();
+	type PostTransactions = ();
 }
 
 parameter_types! {
