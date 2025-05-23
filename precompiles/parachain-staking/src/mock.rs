b846ec2ed985801e6ff4d07aec3a178df3559789
talhadaar <muhammadtalhadar@gmail.com>
2025-05-23 19:01:19 +0500

    (WIP) removed obsolete features and try-runtime-cli package

--- precompiles/parachain-staking/src/mock.rs
+++ precompiles/parachain-staking/src/mock.rs
@@ -132,7 +132,6 @@ impl pallet_aura::Config for Test {
 
 	type AllowMultipleBlocksPerSlot = ConstBool<false>;
 
-	#[cfg(feature = "experimental")]
 	type SlotDuration = ConstU64<SLOT_DURATION>;
 }
 
