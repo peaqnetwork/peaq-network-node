656c439c93659e3571ecd500ddc611a6aeea216e
jaypan <pan@eotlabs.io>
2025-07-15 16:57:22 +0200

    Introduce EVM log

--- primitives/rpc/evm-tracing-events/src/evm.rs
+++ primitives/rpc/evm-tracing-events/src/evm.rs
@@ -223,17 +223,15 @@ impl<'a> From<evm::tracing::Event<'a>> for EvmEvent {
 				is_static,
 				context: context.clone().into(),
 			},
-			// Need backport the evm project
-			// [TODO] In 1.7.2, we don't have this event yet
-			// evm::tracing::Event::Log {
-			// 	address,
-			// 	topics,
-			// 	data,
-			// } => Self::Log {
-			// 	address,
-			// 	topics: topics.to_vec(),
-			// 	data: data.to_vec(),
-			// },
+			evm::tracing::Event::Log {
+				address,
+				topics,
+				data,
+			} => Self::Log {
+				address,
+				topics: topics.to_vec(),
+				data: data.to_vec(),
+			},
 		}
 	}
 }
