9dde947d0d13726e92b77600b2797880bfbdcbd0
Michael Eberhardt <64731211+neutrinoks@users.noreply.github.com>
2025-09-26 12:33:03 +0400

    feat: npm project config for precompile interfaces

---
+++ precompiles/solidity/index.js
@@ -0,0 +1,19 @@
+// peaq-network-node/index.js
+module.exports = {
+  // Export contract addresses or other constants if needed
+  contracts: {
+    interfaces: {
+      asset_erc20: '../assets-erc20/',
+      assets_factory: '../assets-factory/',
+      balances_erc20: '../balances-erc20/',
+      batch: '../batch/',
+      parachain_staking: '../parachain-staking/',
+      peaq_did: '../peaq-did/',
+      peaq_rbac: '../peaq-rbac/',
+      peaq_storage: '../peaq-storage/',
+      vesting: '../vesting/',
+      xcm_utils: '../xcm-utils/',
+      xtokens: '../xtokens/'
+    }
+  }
+};
