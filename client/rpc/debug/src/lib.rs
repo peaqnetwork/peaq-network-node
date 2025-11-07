69580a1d90b3c2964a2f813544fd5701faf726a0
talhadaar <muhammadtalhadar@gmail.com>
2025-11-07 15:17:30 +0500

    cargo fmt

--- client/rpc/debug/src/lib.rs
+++ client/rpc/debug/src/lib.rs
@@ -708,12 +708,11 @@ where
 							// Pre-london update, legacy transactions.
 							match transaction {
 								#[allow(deprecated)]
-								ethereum::TransactionV2::Legacy(tx) =>
-									api.trace_transaction_before_version_4(
-										parent_block_hash,
-										exts,
-										tx,
-									),
+								ethereum::TransactionV2::Legacy(tx) => api.trace_transaction_before_version_4(
+									parent_block_hash,
+									exts,
+									tx,
+								),
 								_ =>
 									return Err(internal_err(
 										"Bug: pre-london runtime expects legacy transactions"
