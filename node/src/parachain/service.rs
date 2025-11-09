6800a326cfcb81e2046cc81c9602dfc388fadecf
jaypan <pan@eotlabs.io>
2025-11-09 06:56:19 +0100

    Fix the cumulus-pallet-weight-reclaim error

--- node/src/parachain/service.rs
+++ node/src/parachain/service.rs
@@ -198,10 +198,11 @@ where
 	let executor = sc_service::new_wasm_executor(&config.executor);
 
 	let (client, backend, keystore_container, task_manager) =
-		sc_service::new_full_parts::<Block, RuntimeApi, _>(
+		sc_service::new_full_parts_record_import::<Block, RuntimeApi, _>(
 			config,
 			telemetry.as_ref().map(|(_, telemetry)| telemetry.handle()),
 			executor,
+			true
 		)?;
 	let client = Arc::new(client);
 
