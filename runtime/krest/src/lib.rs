3294f8578960b169f8f0b58e6f784e9a3a7c04f3
jaypan <pan@eotlabs.io>
2026-07-08 08:03:10 +0200

    Update the krest runtime due to the consistency
    Update the krest network's comment + version even we won't use this anymore, but we can remove
    this runtime in our next release

--- runtime/krest/src/lib.rs
+++ runtime/krest/src/lib.rs
@@ -1,4 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+// Currently, we sunset the krest network. Therefore, after this release, we can remove the
+// krest runtime module to speed up the build time.
+
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit = "256"]
 
@@ -183,10 +186,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
-	spec_version: 107,
+	spec_version: 108,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 1,
+	transaction_version: 2,
 	system_version: 1,
 };
 
