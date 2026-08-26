4d6cfd32aee1d7770677219d6dd017ee952f7c04
Michael Eberhardt <64731211+neutrinoks@users.noreply.github.com>
2026-08-26 17:55:14 +0500

    feat: adapt benchmarking feature for block reward pallet

--- runtime/peaq/src/lib.rs
+++ runtime/peaq/src/lib.rs
@@ -2,6 +2,8 @@
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit = "256"]
 
+extern crate alloc;
+
 // Make the WASM binary available.
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
@@ -2227,7 +2229,7 @@ impl_runtime_apis! {
 
 		fn dispatch_benchmark(
 			config: frame_benchmarking::BenchmarkConfig
-		) -> Result<Vec<frame_benchmarking::BenchmarkBatch>, sp_runtime::RuntimeString> {
+		) -> Result<Vec<frame_benchmarking::BenchmarkBatch>, alloc::string::String> {
 			use frame_benchmarking::{baseline, BenchmarkBatch};
 			use frame_support::traits::TrackedStorageKey;
 			use frame_system_benchmarking::Pallet as SystemBench;
